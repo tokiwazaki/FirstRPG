15fb1cedda48a5756bb63c5331bd279b1a1dca4d
ruruvi <unity.ruruvi@outlook.jp>
2020-08-25 01:19:55 +0900

    add

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.forward * 0.1f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.forward * 0.1f;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position -= transform.right * 0.1f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += transform.right * 0.1f;
+        }
+    }
+}
